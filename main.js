5e9fa7c709b37f5f154e666306c6b0656fa386fc
Mohamad Mourad <89941937+Mohamadmourad@users.noreply.github.com>
2023-09-21 13:41:48 +0300

    all section completed

---
+++ main.js
@@ -0,0 +1,37 @@
+document.getElementById('project1').onclick = () =>{
+    window.open('https://scheduler511.netlify.app/','_blank')
+}
+document.getElementById('codeProject1').onclick = () =>{
+    window.open('https://github.com/Mohamadmourad/scheduler','_blank')
+}
+
+document.getElementById('project2').onclick = () =>{
+    window.open('https://tic-tac-toe-511.netlify.app/','_blank')
+}
+document.getElementById('codeProject2').onclick = () =>{
+    window.open('https://github.com/Mohamadmourad/Tic-Tac-Toe','_blank')
+}
+
+document.getElementById('project3').onclick = () =>{
+    window.open('https://chessguesser.netlify.app/','_blank')
+}
+document.getElementById('codeProject3').onclick = () =>{
+    window.open('https://github.com/Mohamadmourad/chessGuesser','_blank')
+}
+
+document.getElementById('project4').onclick = () =>{
+    window.open('https://tourmaline-banoffee-4d89a0.netlify.app/','_blank')
+}
+document.getElementById('codeProject4').onclick = () =>{
+    window.open('https://github.com/Mohamadmourad/Interactive-rating-component','_blank')
+}
+
+document.getElementById('insta').onclick = () =>{
+    window.open('https://www.instagram.com/mourad511_/','_blank')
+}
+
+document.getElementById('github').onclick = () =>{
+    window.open('https://github.com/Mohamadmourad','_blank')
+}
+
+
